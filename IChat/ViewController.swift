0619cf14f89050b5af6d9ec4d5dab57acf0e6d60
Konstantin-vetro <konstantin.vetro@yandex.ru>
2025-10-18 01:02:32 +0500

    Удалил сториборд, удалил упоминание сториборда в info.plist, в делегате сцены задал чейн контроллер, задал базовый цвет главному контроллеру

--- IChat/ViewController.swift
+++ IChat/ViewController.swift
@@ -2,8 +2,6 @@
 //  ViewController.swift
 //  IChat
 //
-//  Created by Гость on 18.10.2025.
-//
 
 import UIKit
 
@@ -11,9 +9,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .green
     }
-
-
 }
 
