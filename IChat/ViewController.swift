5acf49f1a09dbeb7ebd8b3e3aa6439180732e83b
Konstantin-vetro <konstantin.vetro@yandex.ru>
2025-10-20 23:03:13 +0500

    добавил SwiftUI Preview, для удобства дальнейшей верстки

--- IChat/ViewController.swift
+++ IChat/ViewController.swift
@@ -9,7 +9,27 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .green
+        view.backgroundColor = .red
     }
 }
 
+import SwiftUI
+
+struct ViewControllerProvider: PreviewProvider {
+    static var previews: some View {
+        ContainerView().edgesIgnoringSafeArea(.all)
+    }
+
+    struct ContainerView: UIViewControllerRepresentable {
+
+        let viewController = ViewController()
+
+        func makeUIViewController(context: Context) -> ViewController {
+            return viewController
+        }
+
+        func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+
+        }
+    }
+}
