c9b762766b39119de745c12c327e2ce8114b5ba5
Konstantin-vetro <konstantin.vetro@yandex.ru>
2025-10-24 02:00:10 +0500

    в расширении кнопки добавил значок

--- IChat/Helpers/UIButton + Extension.swift
+++ IChat/Helpers/UIButton + Extension.swift
@@ -33,4 +33,13 @@ extension UIButton {
             self.layer.shadowOffset = CGSize(width: 0, height: 4)
         }
     }
+
+    func customizeGoogleButton() {
+        let googleImage = UIImage(named: "googleLogo")
+        let googleLogo = UIImageView(image: googleImage, contentMode: .scaleAspectFit)
+        googleLogo.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(googleLogo)
+        googleLogo.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 24).isActive = true
+        googleLogo.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+    }
 }
