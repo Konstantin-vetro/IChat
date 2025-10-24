dd29b2275b9eb364d725db3343d9de31a7f55f28
Konstantin-vetro <konstantin.vetro@yandex.ru>
2025-10-24 22:31:11 +0500

    Добавил расширение к сегментот контролю для градации пола

---
+++ IChat/Helpers/SegmentedControl + Extension.swift
@@ -0,0 +1,18 @@
+//
+//  SegmentedControl + Extension.swift
+//  IChat
+//
+//  Created by Гость on 24.10.2025.
+//
+
+import UIKit
+
+extension UISegmentedControl {
+
+    convenience init(first: String, second: String) {
+        self.init()
+        self.insertSegment(withTitle: first, at: 0, animated: true)
+        self.insertSegment(withTitle: second, at: 1 , animated: true)
+        self.selectedSegmentIndex = 0
+    }
+}
