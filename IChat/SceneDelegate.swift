3a7e2123637e3844fe3da757998130b72f7ca519
Konstantin-vetro <konstantin.vetro@yandex.ru>
2025-10-25 23:21:14 +0500

    подправил SceneDelegate, точка входа теперь таббар

--- IChat/SceneDelegate.swift
+++ IChat/SceneDelegate.swift
@@ -13,11 +13,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
-        let mainVC = AuthViewController()
-        let navigationController = UINavigationController(rootViewController: mainVC)
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = navigationController
+        window?.rootViewController = MainTabbarController()
         window?.makeKeyAndVisible()
     }
 
