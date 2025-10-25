1c6bbf817485055a8974a9249a91fb63d1a9ede9
Konstantin-vetro <konstantin.vetro@yandex.ru>
2025-10-25 23:14:34 +0500

    Создал экран PeopleVC

---
+++ IChat/Controllers/PeopleViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PeopleViewController.swift
+//  IChat
+//
+//  Created by Гость on 25.10.2025.
+//
+
+import UIKit
+
+final class PeopleViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+    }
+}
