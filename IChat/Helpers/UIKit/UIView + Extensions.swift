daa3eb6cd9b8c48ba588abd067516c3f28b98f73
Konstantin-vetro <konstantin.vetro@yandex.ru>
2026-01-21 00:06:17 +0500

    create extentions for UIView

---
+++ IChat/Helpers/UIKit/UIView + Extensions.swift
@@ -0,0 +1,20 @@
+//
+//  UIView + Extensions.swift
+//  IChat
+//
+//  Created by Гость on 20.01.2026.
+//
+
+import UIKit
+
+extension UIView {
+    func applyGradients(cornerRadius: CGFloat) {
+        self.backgroundColor = nil
+        self.layoutIfNeeded()
+        let gradientView = GradientView(from: .topTrailing, to: .bottomLeading, startColor: .gradientColorFrom, endColor: .gradientColorTo)
+        if let gradientLayer = gradientView.layer.sublayers?.first as? CAGradientLayer {
+            gradientLayer.frame = self.bounds
+            gradientLayer.cornerRadius = cornerRadius
+        }
+    }
+}
