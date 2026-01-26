23235044eaf31b7cc614a9b3e53ba3573798dd67
Konstantin-vetro <konstantin.vetro@yandex.ru>
2026-01-26 23:14:44 +0500

    fixed name colors

--- IChat/Helpers/UIKit/UIColor + Extension.swift
+++ IChat/Helpers/UIKit/UIColor + Extension.swift
@@ -8,11 +8,11 @@
 import UIKit
 
 extension UIColor {
-    static var buttonRed: UIColor { UIColor(named: "mainWhite") ?? UIColor.red }
+    static var buttonRed: UIColor { UIColor(named: "buttonRed") ?? UIColor.red }
     static var mainWhite: UIColor { UIColor(named: "mainWhite") ?? UIColor.white }
-    static var blackDark: UIColor { UIColor(named: "mainWhite") ?? UIColor.darkGray }
-    static var textFieldLight: UIColor { UIColor(named: "mainWhite") ?? UIColor.lightGray }
-    static var gradientColorFrom: UIColor { UIColor(named: "gradientColorFrom") ?? UIColor.systemPurple }
-    static var gradientColorTo: UIColor { UIColor(named: "gradientColorTo") ?? UIColor.systemBlue }
+    static var blackDark: UIColor { UIColor(named: "blackDark") ?? UIColor.darkGray }
+    static var textFieldLight: UIColor { UIColor(named: "textFieldLight") ?? UIColor.lightGray }
+    static var gradientColorFrom: UIColor { UIColor(named: "GradientColorFrom") ?? UIColor.systemPurple }
+    static var gradientColorTo: UIColor { UIColor(named: "GradientColorTo") ?? UIColor.systemBlue }
     static var shadowColorCell: UIColor { UIColor(named: "shadowColorCell") ?? UIColor.gray }
 }
